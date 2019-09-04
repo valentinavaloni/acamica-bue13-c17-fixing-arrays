4e74cce9fcccb0819156d7c6e4f6be4575e2ce5c
Christian Nahuel Balsamo <bysouleater@gmail.com>
2019-09-03 21:38:46 -0300

    ejercicios

---
+++ js/ejercicio2.js
@@ -0,0 +1,9 @@
+console.log('Ejercicio 2: ');
+
+var misSeries = ['Friends', 'Two & A Half Men', 'How I met your mother', 'The Big Bang Theory'];
+
+console.log('Mi array contiene los series que vi ultimamente: ' + misSeries);
+
+console.log('La ultima que vi fue: ' + misSeries(3));
+console.log('La anterior a esa: ' + misSeries[4]);
+console.log('La primera de mi lista es: ' + misSeries[1]);
---
+++ js/ejercicio4.js
@@ -0,0 +1,19 @@
+console.log('Ejercicio 4: ');
+
+var misLibros = ['Harry Potter', 'Game of Thrones', 'Sherlock', 'Dracula'];
+
+console.log('No recuerdo en que posicion de mi biblioteca deje el libro de Sherlock. ' + misLibros);
+
+var posicion = 0;
+
+console.log('Estaba en la posicion: ' + posicion);
+
+console.log('Porque en verdad solo era la tapa, adentro esta el Libro de la Selva.');
+
+misLibros[] = 'Libro de la Selva';
+
+console.log('Me regalaron para navidad el libro de La divina comedia. La voy a agregar al fondo de mi biblioteca');
+
+
+
+console.log('Ahora si, finalmente mi biblioteca quedo completa: ' + misLibros);
